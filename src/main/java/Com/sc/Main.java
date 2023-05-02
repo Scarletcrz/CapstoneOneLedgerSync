08ff69149f34dfb27dedc98bc4fff5d2aca34473
Scarlet Cruz <scruz@appdev.yearup.org>
2023-05-02 11:06:11 -0400

    adding menus

--- src/main/java/Com/sc/Main.java
+++ src/main/java/Com/sc/Main.java
@@ -1,7 +1,49 @@
 package Com.sc;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+//import java.IOException;
+import java.util.ArrayList;
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        homeScreen();
+        String input;
+        do{
+            System.out.println("Welcome to the Accounting Ledger. Please choose an option below:");
+            System.out.println("\t 1) Add Deposit.");
+            System.out.println("\t 2) Make Payment.");
+            System.out.println("\t 3) Ledger");
+            System.out.println("\t 4) Exit.");
+            System.out.println("Command");
+
+            input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+                    addDeposit();
+                    break;
+                case "2":
+                    makePayment();
+                    break;
+                case "3":
+                    diplaysLedgerScreen();
+                    break;
+                case "4":
+                    System.out.println("Exiting");
+            }
+        } while(!input.equalsIgnoreCase("4"));
+}
+    public static void homeScreen(){
+
+    }
+    public static void addDeposit(){
+
     }
 }
+
+
---
+++ src/main/java/Com/sc/Transaction.java
@@ -0,0 +1,69 @@
+package Com.sc;
+
+public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double addDepositAmount;
+    public Transaction(String date, String time, String description, String vendor, double addDepositAmount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.addDepositAmount = addDepositAmount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAddDepositAmount() {
+        return addDepositAmount;
+    }
+
+
+    public void setAddDepositAmount(double addDepositAmount) {
+        this.addDepositAmount = addDepositAmount;
+
+
+    }
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", addDepositAmount=" + addDepositAmount +
+                '}';
+    }
+}
